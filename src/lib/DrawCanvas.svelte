dd57f297ec549fbae954ac1fd1456f2de809370f
Luna Pixu <officialtrxd@gmail.com>
2025-11-29 19:17:30 +0000

    Added rudimentary routing node visualiser

--- src/lib/DrawCanvas.svelte
+++ src/lib/DrawCanvas.svelte
@@ -1,10 +1,10 @@
 <script lang=ts>
     import { onMount } from "svelte";
-	import { Canvas, PencilBrush } from "fabric";
+	import { Canvas, Circle, FabricObject, PencilBrush, Point, Polyline } from "fabric";
 
 	var canvas : HTMLCanvasElement;
 	var usercolour : string = $state("#000000");
-	var userwidth : number = $state(1);
+	var userwidth : number = $state(4);
 	
 	var fab : Canvas;
 
@@ -25,6 +25,76 @@
 	function Reset() {
 		fab.clear();
 	}
+
+	function RenderGraph() {
+		let spacer = 20;
+		let height = fab.height;
+		let width = fab.width;
+		let points: Circle[][] = [[]];
+		let x = 0;
+		let y = 0;
+		for (let i = spacer / 2; i < height; i += spacer) {
+			points[y] = [];
+			for (let j = spacer / 2; j < width; j += spacer) {
+				let circle = new Circle();
+				circle.setXY(new Point(j, i));
+				circle.backgroundColor = "rgb(255,0,0)";
+				circle.scale(4);
+				fab.add(circle);
+				points[y].push(circle);
+				x++;
+			}
+			y++;
+		}
+
+		for(let y = 0; y < points.length; y++) {
+			for(let x = 0; x < points[y].length; x++) {
+				DrawValidEdges(x, y, points);
+			}
+		}
+		fab.renderAll();
+	}
+
+	function DrawValidEdges(x: number, y: number, points: Circle[][]) {
+		let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+		ctx.getContextAttributes().willReadFrequently = true;
+		let img = ctx.getImageData(0, 0, 400, 400) as ImageData;
+		for (let yOffset = -1; yOffset < 2; yOffset++) {
+			let destY = y + yOffset;
+			if (destY < 0 || destY >= points.length) break;
+			for (let xOffset = -1; xOffset < 2; xOffset++) {
+				let destX = x + xOffset;
+				if (destX < 0 || destX >= points[y].length) break;
+				if (xOffset == 0 && yOffset == 0) continue;
+
+				let origin = points[y][x];
+				let dest = points[destY][destX];
+
+				let isObstructed: boolean = !Raycast(origin.getXY(), dest.getXY(), xOffset, yOffset, img);
+				if (isObstructed) continue;
+
+				let line = new Polyline([origin.getXY(), dest.getXY()]);
+				line.stroke = "rgb(0,255,0)";
+				line.strokeWidth = 2;
+
+				fab.add(line);
+			}
+		}
+	}
+
+	function Raycast(origin: Point, dest: Point, matX: number, matY: number, imgData: ImageData): boolean {
+		let x = origin.x;
+		let y = origin.y;
+		while (x != dest.x || y != dest.y) {
+			if (imgData.data[3 + 4 * (x + y * 400)] > 0) {
+				return false;
+			}
+			x = x == dest.x ? x : x + matX;
+			y = y == dest.y ? y : y + matY;
+			if (x > 400 || y > 400 || x < 0 || y < 0) return false;
+		}
+		return true;
+	}
 </script>
 
 <div id=bundle>
@@ -40,6 +110,7 @@
 		</div>
 		<div class=void></div>
 		<button onclick={Reset}>Reset</button>
+		<button onclick={RenderGraph}>Render Nodes and Edges</button>
 	</div>
 </div>
 
